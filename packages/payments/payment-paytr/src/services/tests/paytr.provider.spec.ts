4bd9de922fe3ca91e6002d8a9291904889cc07db
adrien2p <adrien.deperetti@gmail.com>
2022-02-21 18:26:42 +0100

    chore: Add github actions for tests pipeline and code quality

--- packages/payments/payment-paytr/src/services/tests/paytr.provider.spec.ts
+++ packages/payments/payment-paytr/src/services/tests/paytr.provider.spec.ts
@@ -1,15 +1,24 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import PayTRProviderService  from "../paytr-provider";
+import PayTRProviderService from "../paytr-provider";
 import { CustomerServiceMock } from "../../__mock__/customer";
 import { TotalsServiceMock } from "../../__mock__/totals";
 import { IdMap } from "medusa-test-utils";
-import { Cart } from "@medusajs/medusa/dist";
-import { MerchantConfig } from "../../types";
+import { CartServiceMock } from "../../__mock__/cart";
 
 const merchantConfig: any = {
-
+    token_endpoint: process.env.TOKEN_ENDPOINT,
+    debug_on: 1,
+    test_mode: 1,
+    max_installment: 0,
+    no_installment: 0,
+    merchant_fail_url: 'http://localhost:3000/pay-tr/success',
+    merchant_ok_url: 'http://localhost:3000/pay-tr/fail',
+    merchant_id: process.env.MERCHANT_ID,
+    merchant_key: process.env.MERCHANT_KEY,
+    merchant_salt: process.env.MERCHANT_SALT,
+    timeout_limit: 30
 };
 
 const cartMockData = {
@@ -55,6 +64,7 @@ describe('PayTrProvider', () => {
         jest.clearAllMocks()
         provider = new PayTRProviderService(
             {
+                cartService: CartServiceMock,
                 customerService: CustomerServiceMock,
                 regionService: RegionServiceMock,
                 totalsService: TotalsServiceMock,
@@ -65,8 +75,7 @@ describe('PayTrProvider', () => {
     });
 
     it('should allow to create a payment', async () => {
-        const payment = await provider.createPayment(cartMockData as Cart);
-        expect(payment.data.token).toBeDefined();
-        expect(payment.data.status).toBe(-1);
+        const token = await provider.generateToken(cartMockData.id);
+        expect(token).toBeDefined();
     });
 });
