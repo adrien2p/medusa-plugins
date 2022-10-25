4aa5f4c02cbc5c2064e10ff3bc3aca4fe6bdf5df
fPolic <frane@medusajs.com>
2022-10-25 13:07:33 +0300

    wip: graphs

--- packages/medusa-plugin-sentry-ui/src/components/graphs/index.tsx
+++ packages/medusa-plugin-sentry-ui/src/components/graphs/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { VictoryArea, VictoryAxis, VictoryChart, VictoryContainer, VictoryLabel } from 'victory';
+import { VictoryArea, VictoryAxis, VictoryChart, VictoryTooltip, VictoryVoronoiContainer } from 'victory';
 
 type BarChartProps = {
 	data: [number, [{ count: number }]][];
+	title: string;
+	subtitle: string;
 };
 
 function BarChart(props: BarChartProps) {
-	const data = props.data.map((d) => ({ timestamp: d[0], value: d[1][0].count }));
-	console.log({ data });
+	const data = props.data?.map((d) => ({ timestamp: d[0], value: d[1][0].count }));
 
 	return (
-		<div>
+		<div
+			style={{
+				marginBottom: 32,
+				boxShadow: '0px 0px 1px rgba(0,0,0,.3)',
+				backgroundColor: 'white',
+				borderRadius: 12,
+			}}
+		>
+			<h3 style={{ fontWeight: 'bold', margin: '12px 16px 0 16px' }}>{props.title}</h3>
+			<div
+				style={{ fontSize: 12, margin: ' 0 16px', color: 'rgba(107, 114, 128)' }}
+				className="text-gray font-small mx-16"
+			>
+				{props.subtitle}
+			</div>
+
 			<svg style={{ height: 0 }}>
 				<defs>
 					<linearGradient id="myGradient" x1="100%" y1="100%" x2="100%" y2="0%">
@@ -19,53 +35,64 @@ function BarChart(props: BarChartProps) {
 					</linearGradient>
 				</defs>
 			</svg>
-			<VictoryChart
-				width={320}
-				height={160}
-				padding={{ top: 12, bottom: 0, right: 12, left: 32 }}
-				containerComponent={
-					<VictoryContainer
-						style={{
-							boxShadow: '0px 0px 1px rgba(0,0,0,.3)',
-							marginBottom: 24,
-							backgroundColor: 'white',
-							borderRadius: 12,
-						}}
-						responsive={false}
-					/>
-				}
-				domain={{ y: [0, 1] }}
-				animate={{ duration: 200 }}
-			>
-				<VictoryArea
-					style={{
-						data: { fill: 'url(#myGradient)', stroke: 'transparent' },
-					}}
-					data={data}
-					x="timestamp"
-					y="value"
-					interpolation="natural"
-				/>
 
-				<VictoryAxis dependentAxis />
-			</VictoryChart>
+			<div style={{ width: 360, height: 160 }}>
+				{data && (
+					<VictoryChart
+						width={360}
+						height={160}
+						padding={{ top: 12, bottom: 0, right: 12, left: 32 }}
+						containerComponent={
+							<VictoryVoronoiContainer
+								labels={(d) => {
+									console.log(d);
+									return 'something';
+								}}
+							/>
+						}
+						domain={{ y: [0, 1] }}
+						animate={{ duration: 200 }}
+					>
+						<VictoryArea
+							labelComponent={<VictoryTooltip flyoutStyle={{ fill: 'white' }} />}
+							style={{
+								data: { fill: 'url(#myGradient)', stroke: '#7C3AED' },
+							}}
+							data={data}
+							x="timestamp"
+							y="value"
+							interpolation="natural"
+						/>
+
+						<VictoryAxis
+							dependentAxis
+							style={{
+								axis: { stroke: 'transparent' },
+								tickLabels: { fill: 'transparent' },
+							}}
+						/>
+					</VictoryChart>
+				)}
+			</div>
 		</div>
 	);
 }
 
 function TransactionStats({ graphData }) {
-	if (!graphData) {
-		return null;
-	}
-
-	console.log(graphData);
-
-	graphData['failure_rate()'].data.forEach((d) => (d[1][0].count = Math.random() > 0.9 ? Math.random() : 0));
+	graphData?.['apdex()']?.data.forEach((d) => (d[1][0].count = Math.random() > 0.9 ? Math.random() : 0));
+	graphData?.['failure_rate()']?.data.forEach((d) => (d[1][0].count = Math.random() > 0.9 ? Math.random() : 0));
+	graphData?.['tpm()']?.data.forEach((d) => (d[1][0].count = Math.random() > 0.9 ? Math.random() : 0));
 
 	return (
-		<>
-			<BarChart data={graphData['failure_rate()'].data} />
-		</>
+		<div className="flex justify-between">
+			<BarChart title="Apdex" subtitle="Long description of the stat" data={graphData?.['apdex()'].data} />
+			<BarChart
+				title="Faliure rate"
+				subtitle="Long description of the stat"
+				data={graphData?.['failure_rate()'].data}
+			/>
+			<BarChart title="TPM" subtitle="Long description of the stat" data={graphData?.['tpm()'].data} />
+		</div>
 	);
 }
 
