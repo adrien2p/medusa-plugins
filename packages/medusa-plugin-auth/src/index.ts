01caad96e616b6f8fc605f414d8c4d8325d76f90
adrien2p <adrien.deperetti@gmail.com>
2022-12-13 18:58:12 +0100

    cleanup

--- packages/medusa-plugin-auth/src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Noop passport session in order to delayed the usage of it after the custom auth strategies have been applied
- */
-import passport from 'passport';
-
-export const originalPassportSession = passport.session;
-passport.session = () => () => void 0;
--- packages/medusa-plugin-auth/src/loaders/index.ts
+++ packages/medusa-plugin-auth/src/loaders/index.ts
@@ -1,11 +1,9 @@
-import passport from 'passport';
 import { ConfigModule, MedusaContainer } from '@medusajs/medusa/dist/types/global';
 
 import { AuthOptions } from '../types';
 import GoogleStrategy from '../auth-strategies/google';
 import FacebookStrategy from '../auth-strategies/facebook';
 import LinkedinStrategy from '../auth-strategies/linkedin';
-import { originalPassportSession } from '../index';
 
 export default async function authStrategiesLoader(container: MedusaContainer, authOptions: AuthOptions) {
 	const configModule = container.resolve('configModule') as ConfigModule;
@@ -13,6 +11,4 @@ export default async function authStrategiesLoader(container: MedusaContainer, a
 	GoogleStrategy.load(container, configModule, authOptions);
 	FacebookStrategy.load(container, configModule, authOptions);
 	LinkedinStrategy.load(container, configModule, authOptions);
-
-	passport.use(originalPassportSession);
 }
