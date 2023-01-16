0bf329d0a3b6b6904c76801f568740af23f6478f
adrien2p <adrien.deperetti@gmail.com>
2023-01-16 22:41:30 +0100

    chore: include the user object in the token

--- packages/medusa-plugin-auth/src/core/auth-callback-middleware.ts
+++ packages/medusa-plugin-auth/src/core/auth-callback-middleware.ts
@@ -15,7 +15,7 @@ export function authCallbackMiddleware(
 	successRedirect: string
 ) {
 	return (req, res) => {
-		const tokenData = domain === 'admin' ? { userId: req.user.id } : { customer_id: req.user.id };
+		const tokenData = domain === 'admin' ? { userId: req.user.id, ...req.user } : { customer_id: req.user.id, ...req.user };
 		const token = jwt.sign(tokenData, secret, { expiresIn });
 		const sessionKey = domain === 'admin' ? 'jwt' : 'jwt_store';
 		req.session[sessionKey] = token;
