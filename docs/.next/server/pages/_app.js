b3aa15321a3a9d065919e831b20e6827703ea801
adrien2p <adrien.deperetti@gmail.com>
2023-01-26 14:12:37 +0100

    docs: Start new documentation

---
+++ docs/.next/server/pages/_app.js
@@ -0,0 +1,57 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/_app";
+exports.ids = ["pages/_app"];
+exports.modules = {
+
+/***/ "./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ MyApp)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles.css */ \"./styles.css\");\n/* harmony import */ var _styles_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_css__WEBPACK_IMPORTED_MODULE_1__);\n\n\nfunction MyApp({ Component , pageProps  }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n        ...pageProps\n    }, void 0, false, {\n        fileName: \"/Users/adriendeperetti/Documents/02-personnel/01-projects/medusa-plugins/docs/pages/_app.js\",\n        lineNumber: 4,\n        columnNumber: 10\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7OztBQUFzQjtBQUVQLFNBQVNBLE1BQU0sRUFBRUMsVUFBUyxFQUFFQyxVQUFTLEVBQUUsRUFBRTtJQUN0RCxxQkFBTyw4REFBQ0Q7UUFBVyxHQUFHQyxTQUFTOzs7Ozs7QUFDakMsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL0BtZWR1c2EtcGx1Z2lucy9tZWR1c2EtcGx1Z2lucy1kb2NzLy4vcGFnZXMvX2FwcC5qcz9lMGFkIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnLi4vc3R5bGVzLmNzcydcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gTXlBcHAoeyBDb21wb25lbnQsIHBhZ2VQcm9wcyB9KSB7XG4gIHJldHVybiA8Q29tcG9uZW50IHsuLi5wYWdlUHJvcHN9IC8+XG59Il0sIm5hbWVzIjpbIk15QXBwIiwiQ29tcG9uZW50IiwicGFnZVByb3BzIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
+
+/***/ }),
+
+/***/ "./styles.css":
+/*!********************!*\
+  !*** ./styles.css ***!
+  \********************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/_app.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ docs/theme.config.jsx
@@ -0,0 +1,86 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+
+export default {
+  project: {
+    link: 'https://github.com/adrien2p/medusa-plugins',
+  },
+
+  chat: {
+    link: 'https://discord.gg/xpCwq3Kfn8',
+  },
+
+  navigation: {
+    prev: true,
+    next: true
+  },
+
+	footer: {
+    text: () => {
+      return (
+        <span>
+          MIT {new Date().getFullYear()} © <a href="https://nextra.site" target="_blank">Medusa plugins</a>.
+          <div style={{display: "flex", marginTop: "0.5rem"}}>
+            <a href="https://github.com/adrien2p/awesome-medusajs"><img alt="Awesome medusajs" src="https://awesome.re/badge.svg" height="20"/></a>
+            <a style={{marginLeft: "0.5rem"}} href="https://discord.gg/xpCwq3Kfn8"><img alt="Discord" src="https://img.shields.io/badge/chat-on%20discord-7289DA.svg" height="20"/></a>
+            <a style={{marginLeft: "0.5rem"}} href="https://github.com/adrien2p/medusa-plugins/commits/main"><img alt="Activity" src="https://img.shields.io/github/commit-activity/m/adrien2p/medusa-plugins?style=flat" height="20"/></a>
+            <a style={{marginLeft: "0.5rem"}} href="https://github.com/adrien2p/medusa-plugins/issues"><img alt="Issues" src="https://img.shields.io/github/issues/adrien2p/medusa-plugins?style=flat" height="20"/></a>
+            <a style={{marginLeft: "0.5rem"}} href="https://github.com/adrien2p/medusa-plugins/blob/main/LICENSE"><img alt="Licence" src="https://img.shields.io/github/license/adrien2p/medusa-plugins?style=flat" height="20"/></a>
+            <a style={{marginLeft: "0.5rem"}} href="https://github.com/adrien2p/medusa-plugins/blob/main/CONTRIBUTING.md"><img alt="Contributing" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat" height="20"/></a>
+            <a style={{marginLeft: "0.5rem"}} href="https://github.com/sponsors/adrien2p"><img alt="sponsor" src="https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=GitHub&color=%23fe8e86" height="20"/></a>
+          </div>
+        </span>
+      )
+    },
+  },
+
+  // Dark mode related
+  darkMode: true,
+
+  // Edit this page and feedback related
+  docsRepositoryBase: "https://github.com/adrien2p/medusa-plugins/tree/main/docs/",
+
+  // SEO related
+  useNextSeoProps() {
+    return {
+      titleTemplate: 'Medusa plugins - %s'
+    }
+  },
+
+  // HEAD related
+  head: () => {
+    const { asPath } = useRouter()
+    const { frontMatter } = useConfig()
+
+    return <>
+      <meta property="og:url" content={`https://my-app.com${asPath}`} />
+      <meta property="og:title" content={frontMatter.title || 'Medusa plugins'} />
+      <meta property="og:description" content={frontMatter.description || 'A collection of awesome plugins for medusa'} />
+    </>
+  },
+
+  // Navbar related
+  logo: (
+    <>
+      <img src="https://raw.githubusercontent.com/adrien2p/medusa-plugins/assets/assets/medusa-plugins-logo.png" alt="medusa plugins logo" height="24" width="24"/>
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+        Medusa plugins
+      </span>
+    </>
+  ),
+
+  // Custom components
+  components: {
+    CircleStep: (props) => {
+      const index = props.index
+      return (
+        <div style={{display: "flex", marginTop: "1rem"}}>
+          <span className={"circle"}>{index}</span>
+          <span style={{marginTop: "5px"}}>
+            {props.children}
+          </span>
+        </div>
+      )
+    }
+  }
+}
