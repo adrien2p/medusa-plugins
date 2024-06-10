3b3b658e9d7822023abd3dbf07419f9b2d9f9f45
fwilke <fredrik.wilke@filiago.net>
2024-06-10 19:22:35 -0400

    fix: admin.scope and store.scope parameters from medusa-config.js not passed through to passport

--- packages/medusa-plugin-auth/src/auth-strategies/azure-oidc/store.ts
+++ packages/medusa-plugin-auth/src/auth-strategies/azure-oidc/store.ts
@@ -33,6 +33,7 @@ export function getAzureStoreStrategy(id: string): StrategyFactory<AzureAuthOpti
 				isB2C: strategyOptions.store.isB2C ?? false,
 				issuer: strategyOptions.store.issuer,
 				passReqToCallback: true,
+				scope: strategyOptions.admin.scope,
 			} as IOIDCStrategyOptionWithRequest);
 		}
 
